aa1c4068def8d503f8984a98622812eb6be18ece
Max Tkachenko <makseq@gmail.com>
2021-10-19 22:08:41 +0300

    Virtual Tabs and TaskListAPI with query params (#1579)
    * TaskListAPI with query params
    
    * Fix trigram ext for migrations
    
    * Fix with project/views
    
    * Fix view get
    
    * Fix tests
    
    * Fix tests more
    
    * Fix View permissions
    
    * Update react-app and dm2 virtual tabs functional
    
    * [dm] Add hiddenColumns to virtual tab query
    
    * Fix tests
    
    Co-authored-by: Koshevarov Sergey <gondragos@gmail.com>

--- label_studio/frontend/dist/react-app/index.js
+++ label_studio/frontend/dist/react-app/index.js
@@ -20884,7 +20884,7 @@ const APIConfig = {
     task: "/tasks/:taskID",
 
     /** List of tasks (samples) in the dataset */
-    tasks: "/views/:tabID/tasks",
+    tasks: "/tasks",
 
     /** Next task (labelstream, default sequential) */
     nextTask: "/tasks/next",
